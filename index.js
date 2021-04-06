e3fc5917e86bc19df848b1d8628aaf298b2b98d9
Fariz Rifqi <farizrifqi26@gmail.com>
2021-04-06 12:34:10 +0800

    updated

--- index.js
+++ index.js
@@ -8,8 +8,14 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '675485146:AAHuyOpGg0QLE33IP5gaBXN8Fx_WKT2pTww';
 const bot = new TelegramBot(token, {polling: true});
 const listAddress = ['zil1zra5yhlgqchr3fn4kj7gaam57h2x6lgrjzmm62', 'zil1dpjypt0m0t3hdvw7mhe9a6vdtmgcys9nlh54gg']
-setInterval(async () => {
+checkTrx()
+setInterval( () => {
+    checkTrx()
+}, 1200000)
+
+async function checkTrx(){
     listAddress.forEach(async addr => {
+        console.log(addr)
         let result = await fetch(`https://api.micromedia.id/v1/zilliqa/transactions/?address=${addr}`)
         let resultJson = await result.json()
         if (resultJson['status'] == "200"){
@@ -21,7 +27,8 @@ setInterval(async () => {
                         console.log(trx)
                         let isSuccess = trx['receiptSuccess'] ? 'Success' : 'Failed'
                         let messageText = `[ ${trx['to']} ]\n\nHash: ${trx['hash']}\nFrom:${trx['from']}\nStatus: ${isSuccess}\n===================================\n`
-                        return bot.sendMessage("-1001285503524", messageText);
+                        bot.sendMessage("-1001285503524", messageText);
+                        return;
                     }else{
                         return;
                     }
@@ -29,4 +36,4 @@ setInterval(async () => {
             }
         }
     })
-}, 1800000)
+}
